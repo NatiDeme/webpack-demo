8f47e04a3894f9a1682ba51c5dd1ff29ad5928e1
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-06-03 16:12:44 +0300

    final fix to clear button functionality

--- src/modules/completedChecker.js
+++ src/modules/completedChecker.js
@@ -1,11 +1,12 @@
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 const completedChecker = () => {
   const clearButton = document.getElementById('clear');
   clearButton.addEventListener('click', () => {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const newTasks = tasks.filter((tasks) => tasks.completed === false);
     for (let i = 0; i < newTasks.length; i += 1) {
       newTasks[i].index = i;
     }
+    console.log(newTasks);
     localStorage.setItem('tasks', JSON.stringify(newTasks));
     window.location.reload();
   });
